9d955cdd7544635ec06faf117ff25a24605bda44
GAMELOFT\long.lethanh3 <long.lethanh3@gameloft.com>
2022-07-08 19:08:03 +0700

    +Add joystick controller + Add Player class

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Singleton
+    public static Player Instance { get; private set; }
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    //How to get method in Player 
+    //Player.Instance.MethodName(Param1,Param2);
+
+    //Attribute of Player
+    private bool isTakeOrder = false;
+    private bool isInGreenArea = false;
+    private bool isInRedArea = false;
+
+    public float MoodIndex = 5.0f;    //Max = 10, Min = 0
+
+    //Method
+    public void MoodIndexIncrease()
+    {
+        MoodIndex += 0.2f;
+    }
+    public void MoodIndexDecrease()
+    {
+        MoodIndex -= 0.2f;
+    }
+
+
+    private void Update()
+    {
+        if (isInGreenArea)
+        {
+            MoodIndexDecrease();
+        }
+        if (isInRedArea)
+        {
+            MoodIndexIncrease();
+        }
+        if (MoodIndex >= 10)
+        {
+            //GameOver BLOOM!
+        }
+    }
+}
