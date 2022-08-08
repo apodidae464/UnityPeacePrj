fe56dc502fb7a1666f12916df691a36e9d758b11
apodidae464 <kimikissmicki@gmail.com>
2022-08-08 12:16:27 +0700

    more customer sprite

---
+++ Assets/Script/src/CustomerSpite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpite : MonoBehaviour
+{
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] list;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int temp = Random.Range(0, list.Length);
+        spriteRenderer.sprite = list[temp];
+    }
+
+    private void OnEnable()
+    {
+        int temp = Random.Range(0, list.Length);
+        spriteRenderer.sprite = list[temp];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
